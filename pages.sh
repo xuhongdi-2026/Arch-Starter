d2ab3a3e1aab2eb4de9b5099bdc6d8c1d1b8d6a7
xuhongdi <xuhongdi_2026@outlook.com>
2026-03-29 08:25:58 +0800

    aaa

--- pages.sh
+++ pages.sh
@@ -2,12 +2,8 @@
 
 # Cloudflare pages
 
-if [ python3 --version && mkdir ./.venv ];then
-    python3 -m venv ./.venv
-fi
-if [ pip --version ];then
-    pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/simple
-    pip install mkdocs-material
-fi
+python3 -m venv ./.venv
+pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/simple
+pip install mkdocs-material
 
 mkdocs build
